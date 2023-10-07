2ac342461196fb43fc2cb5186243c2ff826564b9
TONGTONGDESHIJIE <1090060206@qq.com>
2023-10-07 10:02:53 +0800

    初始化项目

---
+++ src/test/java/top/chen/share/ShareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package top.chen.share;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
